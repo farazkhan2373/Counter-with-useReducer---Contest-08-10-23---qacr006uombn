73475571ec17c946f0262b25f1bb612804c451d4
farazkhan2373 <136571344+farazkhan2373@users.noreply.github.com>
2023-10-08 18:17:55 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,12 @@
 const counterReducer = (state,action) =>{
-
+switch (action.type) {
+    case "incrementing":
+      return { count: state.count + 1 };
+    case "decrementing":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
 }
 
 export {counterReducer}
